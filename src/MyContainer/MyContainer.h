be90781dc433fcfa6bc82286e5483c33f820dbb9
Vitaly Senokosov <6545415@gmail.com>
2021-10-31 19:30:23 +0300

    add typename TAllocator into template

--- src/MyContainer/MyContainer.h
+++ src/MyContainer/MyContainer.h
@@ -3,17 +3,21 @@
 #include <cstdlib>
 #include <memory>
 
-template<typename T>
+template<typename T, typename TAllocator = std::allocator<T>>
 class MyContainer {
 private:
     T *arr;
     size_t size{};
     size_t capacity;
+    TAllocator allocator;
+
+    using AllocatorTraits = std::allocator_traits<TAllocator>;
 
     void DeleteArr() const;
 
 public:
     MyContainer();
+    explicit MyContainer(size_t, const T& value = T(), TAllocator alloc = TAllocator());
     virtual ~MyContainer();
     void reserve(size_t n);
     void resize(size_t n, const T &value = T());
@@ -25,19 +29,25 @@ public:
     const T &operator[](size_t index) const;
 };
 
-template<typename T>
-MyContainer<T>::MyContainer() {
+template<typename T, typename TAllocator>
+MyContainer<T, TAllocator>::MyContainer() {
     arr = reinterpret_cast<T *>(malloc(sizeof(T)));
     capacity = 1;
 }
 
-template<typename T>
-MyContainer<T>::~MyContainer() {
+template<typename T, typename TAllocator>
+MyContainer<T, TAllocator>::MyContainer(size_t n, const T& value, const TAllocator alloc) {
+    arr = reinterpret_cast<T *>(malloc(sizeof(T)));
+    capacity = 1;
+}
+
+template<typename T, typename TAllocator>
+MyContainer<T, TAllocator>::~MyContainer() {
     DeleteArr();
 }
 
-template<typename T>
-void MyContainer<T>::reserve(size_t n) {
+template<typename T, typename TAllocator>
+void MyContainer<T, TAllocator>::reserve(size_t n) {
     if (capacity == 0) {
     }
 
@@ -61,8 +71,8 @@ void MyContainer<T>::reserve(size_t n) {
 
 }
 
-template<typename T>
-void MyContainer<T>::resize(size_t n, const T &value) {
+template<typename T, typename TAllocator>
+void MyContainer<T, TAllocator>::resize(size_t n, const T &value) {
     if (n > capacity) {
         reserve(n);
     }
@@ -76,8 +86,8 @@ void MyContainer<T>::resize(size_t n, const T &value) {
     }
 }
 
-template<typename T>
-void MyContainer<T>::push_back(const T &value) {
+template<typename T, typename TAllocator>
+void MyContainer<T, TAllocator>::push_back(const T &value) {
     if (capacity == size) {
         reserve(2 * size);
     }
@@ -86,26 +96,26 @@ void MyContainer<T>::push_back(const T &value) {
     ++size;
 }
 
-template<typename T>
-T &MyContainer<T>::at(size_t index) {
+template<typename T, typename TAllocator>
+T &MyContainer<T, TAllocator>::at(size_t index) {
     if (index >= size) {
         throw std::out_of_range("Attempt out of range access");
     }
     return arr[index];
 }
 
-template<typename T>
-T &MyContainer<T>::operator[](size_t index) {
+template<typename T, typename TAllocator>
+T &MyContainer<T, TAllocator>::operator[](size_t index) {
     return arr[index];
 }
 
-template<typename T>
-const T &MyContainer<T>::operator[](size_t index) const {
+template<typename T, typename TAllocator>
+const T &MyContainer<T, TAllocator>::operator[](size_t index) const {
     return arr[index];
 }
 
-template<typename T>
-void MyContainer<T>::DeleteArr() const {
+template<typename T, typename TAllocator>
+void MyContainer<T, TAllocator>::DeleteArr() const {
     for (size_t i = 0; i < this->size; ++i) {
         this->arr[i].~T();
     }
