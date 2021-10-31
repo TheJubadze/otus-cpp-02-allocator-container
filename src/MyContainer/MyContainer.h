6c56b1100547c47c2df3ecd12ae56f476314cbcc
Vitaly <6545415@gmail.com>
2021-11-01 00:11:57 +0300

    substitute new/delete with allocator.allocate/deallocate

--- src/MyContainer/MyContainer.h
+++ src/MyContainer/MyContainer.h
@@ -5,40 +5,40 @@
 #include "Core.h"
 
 template<typename T, typename TAllocator = std::allocator<T>>
-class API MyContainer {
-private:
-    T *arr;
-    size_t size{};
-    size_t capacity;
-    TAllocator allocator;
+class API MyContainer{
+        private:
+        T *arr;
+        size_t size{};
+        size_t capacity;
+        TAllocator allocator;
 
-    using AllocatorTraits = std::allocator_traits<TAllocator>;
+        using AllocatorTraits = std::allocator_traits<TAllocator>;
 
-    void DeleteArr() const;
+        void DeleteArr();
 
-public:
-    MyContainer();
-    explicit MyContainer(size_t, const T& value = T(), TAllocator alloc = TAllocator());
-    virtual ~MyContainer();
-    void reserve(size_t n);
-    void resize(size_t n, const T &value = T());
-    void push_back(const T &value);
+        public:
+        MyContainer();
+        explicit MyContainer(size_t, const T& value = T(), TAllocator alloc = TAllocator());
+        virtual ~MyContainer();
+        void reserve(size_t n);
+        void resize(size_t n, const T &value = T());
+        void push_back(const T &value);
 
-    T &at(size_t index);
+        T &at(size_t index);
 
-    T &operator[](size_t index);
-    const T &operator[](size_t index) const;
+        T &operator[](size_t index);
+        const T &operator[](size_t index) const;
 };
 
 template<typename T, typename TAllocator>
 MyContainer<T, TAllocator>::MyContainer() {
-    arr = reinterpret_cast<T *>(allocator.allocate(sizeof(T)));
+    arr = allocator.allocate(sizeof(T));
     capacity = 1;
 }
 
 template<typename T, typename TAllocator>
-MyContainer<T, TAllocator>::MyContainer(size_t n, const T& value, const TAllocator alloc) {
-    arr = reinterpret_cast<T *>(allocator.allocate(sizeof(T)));
+MyContainer<T, TAllocator>::MyContainer(size_t n, const T &value, const TAllocator alloc) {
+    arr = allocator.allocate(sizeof(T));
     capacity = 1;
 }
 
@@ -56,12 +56,12 @@ void MyContainer<T, TAllocator>::reserve(size_t n) {
         return;
     }
 
-    T *newarr = reinterpret_cast<T *>(malloc(n * sizeof(T)));
+    T *newarr = allocator.allocate(n * sizeof(T));
 
     try {
         std::uninitialized_copy(arr, arr + size, newarr);
     } catch (...) {
-        delete[] reinterpret_cast<int8_t *>(newarr);
+        allocator.deallocate(newarr, size);
         throw;
     }
 
@@ -116,10 +116,10 @@ const T &MyContainer<T, TAllocator>::operator[](size_t index) const {
 }
 
 template<typename T, typename TAllocator>
-void MyContainer<T, TAllocator>::DeleteArr() const {
+void MyContainer<T, TAllocator>::DeleteArr() {
     for (size_t i = 0; i < this->size; ++i) {
-        this->arr[i].~T();
+        allocator.destroy(arr + i);
     }
 
-    delete[] reinterpret_cast<int8_t *>(this->arr);
+    allocator.deallocate(arr, size);
 }
